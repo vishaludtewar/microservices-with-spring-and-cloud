1740e6e74e128642004dbacfbb8af5845d19b69c
vishivaj <udtewarvish@gamil.com>
2024-02-27 19:21:06 +0530

    updated

---
+++ accounts/src/main/java/com/msibank/accounts/controller/CustomerController.java
@@ -0,0 +1,69 @@
+package com.msibank.accounts.controller;
+
+import com.msibank.accounts.dto.CustomerDetailsDto;
+import com.msibank.accounts.dto.ErrorResponseDto;
+import com.msibank.accounts.service.ICustomersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(
+        name = "REST API for Customers in MSIBank",
+        description = "REST APIs in MSIBank to FETCH customer details"
+)
+@RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
+public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    private final ICustomersService iCustomersService;
+
+    public CustomerController(ICustomersService iCustomersService){
+        this.iCustomersService = iCustomersService;
+    }
+
+    @Operation(
+            summary = "Fetch Customer Details REST API",
+            description = "REST API to fetch Customer details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("msibank-correlation-id")
+                                                                       String correlationId,
+                                                                   @RequestParam @Pattern(regexp="(^$|[0-9]{10})",
+                                                                            message = "Mobile number must be 10 digits")
+                                                                   String mobileNumber) {
+        logger.debug("msiBank-correlation-id found: {} ", correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
+        return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
+
+    }
+
+
+}
