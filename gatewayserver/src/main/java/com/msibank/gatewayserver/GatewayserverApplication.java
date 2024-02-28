3e433ee427d7c4663477aeb2b795abdeb1e3f931
vishivaj <udtewarvish@gamil.com>
2024-02-28 14:55:06 +0530

    updated

--- gatewayserver/src/main/java/com/msibank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/msibank/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.msibank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +15,28 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator msiBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/msibank/accounts/**")
+						.filters( f -> f.rewritePath("/msibank/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/msibank/loans/**")
+						.filters( f -> f.rewritePath("/msibank/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/msibank/cards/**")
+						.filters( f -> f.rewritePath("/msibank/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CARDS")).build();
+
+
+	}
+
+
 }
+
